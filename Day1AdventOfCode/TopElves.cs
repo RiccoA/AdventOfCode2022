3d17fc77f7fc00d23f03570d4cefc32d833807e5
Ricco <ricco.amezcua@gmail.com>
2022-12-05 15:15:01 -0600

    Refactor top elves

--- Day1AdventOfCode/TopElves.cs
+++ Day1AdventOfCode/TopElves.cs
@@ -4,47 +4,38 @@
     {
         public TopElves(int poolSize, IList<Elf> elves)
         {
-            _poolSize = poolSize;
-            _elves = elves.ToList();
-            _topElfTotals = new int[3];
-            FillTopElfTotals();
+            _topElfTotals = new int[poolSize];
+            FillTopElfTotals(elves.ToList());
         }
 
         private int[] _topElfTotals;
 
-        private IList<Elf> _elves { get; init; } = new List<Elf>();
-        private int _poolSize { get; init; }
 
         public int GetTopTotalCalories()
         {
             return _topElfTotals.Sum();
         }
 
-        private void FillTopElfTotals()
+        private void FillTopElfTotals(IEnumerable<Elf> elves)
         {
-            foreach (Elf elf in _elves)
+            foreach (Elf elf in elves)
             {
-                int elfTotalCalorie = elf.GetTotalCalorieCount();
-                if (elfTotalCalorie > _topElfTotals[0])
-                {
-                    int swapValue = _topElfTotals[0];
-                    _topElfTotals[0] = elfTotalCalorie;
-                    elfTotalCalorie = swapValue;
-                }
-                if (elfTotalCalorie > _topElfTotals[1])
-                {
-                    int swapValue = _topElfTotals[1];
-                    _topElfTotals[1] = elfTotalCalorie;
-                    elfTotalCalorie = swapValue;
-                }
-                if (elfTotalCalorie > _topElfTotals[2])
-                {
-                    int swapValue = _topElfTotals[2];
-                    _topElfTotals[2] = elfTotalCalorie;
-                    elfTotalCalorie = swapValue;
-                }
+                UpdateTopElfTotalsWithNewElf(elf);
             }
         }
 
+        private void UpdateTopElfTotalsWithNewElf(Elf elf)
+        {
+            int newValueToCheck = elf.GetTotalCalorieCount();
+            for (int topElfIndex = 0; topElfIndex < _topElfTotals.Length; topElfIndex++)
+            {
+                if (newValueToCheck > _topElfTotals[topElfIndex])
+                {
+                    int swapValue = _topElfTotals[topElfIndex];
+                    _topElfTotals[topElfIndex] = newValueToCheck;
+                    newValueToCheck = swapValue;
+                }
+            }
+        }
     }
 }
