9aa60244d6c69ca84ec60973a26abcbdd2fdaa49
Ricco <ricco.amezcua@gmail.com>
2022-12-05 08:01:02 -0600

    Able to read data from test file

---
+++ Day1AdventOfCode.Tests/IntegrationTests/FileElfSouce.IntegrationTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+
+namespace Day1AdventOfCode.Tests.IntegrationTests
+{
+    public class FileElfSourceIntegrationTests
+    {
+
+        [Fact]
+        public void FileElfSource_LoadsFile_WhenFileIsValid()
+        {
+            var path = "IntegrationTests/Resources/test_data.txt";
+
+            var file = new FileElfSource(path);
+
+            var elves = file.GetElves();
+
+            elves.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public void FileElfSource_HasCorrectTotal_WhenLoadingFile()
+        {
+            var path = "IntegrationTests/Resources/test_data.txt";
+
+            var elfSource = new FileElfSource(path);
+
+            var elves = elfSource.GetElves();
+            var elfGroupFactory = new ElfGroupFactory(elfSource);
+            var elfGroup = elfGroupFactory.CreateGroup();
+
+            elfGroup.GetHighestTotalOfCalories().Should().Be(24000);
+
+
+        }
+
+
+    }
+}
---
+++ Day1AdventOfCode/FileElfSource.cs
@@ -0,0 +1,49 @@
+﻿namespace Day1AdventOfCode
+{
+    public class FileElfSource : IElfSource
+    {
+        private string _path { get; init; }
+
+        public FileElfSource(string path)
+        {
+            _path = path;
+        }
+
+        public IEnumerable<Elf> GetElves()
+        {
+            var elves = new List<Elf>();
+            Elf elf = new();
+
+            foreach (string line in ReadLinesFrom(_path))
+            {
+                if (IsNewElfMarker(line))
+                {
+                    elves.Add(elf);
+                    elf = new Elf();
+                }
+
+                if (IsValidCalorie(line, out int calorieCount))
+                {
+                    elf.AddFoodToSupply(new Food(calorieCount));
+                }
+            }
+
+            return elves;
+        }
+
+        private IEnumerable<string> ReadLinesFrom(string path)
+        {
+            return File.ReadLines(path);
+        }
+
+        private bool IsValidCalorie(string line, out int result)
+        {
+            return int.TryParse(line, out result);
+        }
+
+        private bool IsNewElfMarker(string line)
+        {
+            return line.Equals(string.Empty);
+        }
+    }
+}
